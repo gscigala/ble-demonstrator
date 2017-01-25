f2f5dd0052842e315b278ac4acf35a36d44f281c
Emmanuel Bouillot <emmanuel.bouillot@openwide.fr>
2017-01-25 11:52:50 +0100

    add basic code structure to BLE extern process

--- server/calaos_proc/BLEexternProc_main.cpp
+++ server/calaos_proc/BLEexternProc_main.cpp
@@ -12,10 +12,28 @@ class BLEprocess: public ExternProcClient{
 		virtual ~BLEprocess();
 	
 	protected:
-		string getValue();
+		int getValue(const string &device);
 		list<string> scanDevices();
 		void sendValues();
 
 		virtual void readTimeout();
 		virtual void messageReceived(const string &msg);
 }
+
+int BLEprocess::getValue(){
+	//TODO
+}
+
+void BLEprocess::readTimeout{
+	lisT<string> l = scanDevices();
+
+	Json jdata;
+	for (const string &dev: l){
+		jdata.push_back({
+			{"id", dev},
+			{"value", getValue(dev) },
+		});
+  	}
+
+	sendMessage(jdata.dump());	
+}	
